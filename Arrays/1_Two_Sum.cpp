c4bd85ffd88a24b81c3a3dabbaa0d626d208257b
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-06-27 18:14:05 +0530

    Problem No.: 1. Two Sum

---
+++ Arrays/1_Two_Sum.cpp
@@ -0,0 +1,52 @@
+/* Leetcode Problem No.: 1. Two Sum  */
+/*
+    Company Tags                : Zoho, Flipkart, Morgan Stanley, Accolite, Amazon, Microsoft, FactSet, Hike, Adobe, Google, Wipro, SAP Labs, CarWale
+    Leetcode Link               : https://leetcode.com/problems/two-sum/
+*/
+
+/******************************************************************** C++ ********************************************************************/
+// Approach using HAsh map
+
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i<nums.size(); i++) {
+            if(mp.count(target-nums[i]))
+                return {mp[target-nums[i]], i};
+            mp[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
+
+/******************************************************************** JAVA ********************************************************************/
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int remaining = target - nums[i];
+            
+            if (mp.containsKey(remaining)) {
+                return new int[]{mp.get(remaining), i};
+            }
+            
+            mp.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+
+
+/*
+    YOUTUBE VIDEO ON THIS Qn    : https://www.youtube.com/watch?v=TCaBnVIllrQ
+*/
