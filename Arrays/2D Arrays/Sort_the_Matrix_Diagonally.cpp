500cb8de490e1b3a2fbb43277cf8bee4c69604aa
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-06-28 20:25:10 +0530

    Sort the Matrix Diagonally

--- Arrays/2D Arrays/Sort_the_Matrix_Diagonally.cpp
+++ Arrays/2D Arrays/Sort_the_Matrix_Diagonally.cpp
@@ -14,6 +14,44 @@
     Related Qn : https://leetcode.com/problems/diagonal-traverse/
 */
 
+
+
+
+
+/*
+    class Solution {
+    public:
+        vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+
+            int m = mat.size(); // row
+            int n = mat[0].size(); // col
+
+            unordered_map<int, vector<int>> mp;
+
+            // step 1: store diagonal elements in a map
+            for( int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    mp[i-j].push_back(mat[i][j]);
+                }
+            }
+
+            // Step 2: sort them
+            for (auto &it: mp) {
+                sort(it.second.begin(), it.second.end());
+            }
+
+            // step 3: now, put them back in sorted order
+            for (int i = m -1; i >= 0; i--) {
+                for (int j = n -1; j >= 0; j--) {
+                    mat[i][j] = mp[i-j].back();
+                    mp[i-j].pop_back();
+                }
+            }
+            return mat;
+        }
+    };
+*/
+
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
@@ -47,6 +85,7 @@ public:
         return mat;
     }
 };
+
 
 
 
