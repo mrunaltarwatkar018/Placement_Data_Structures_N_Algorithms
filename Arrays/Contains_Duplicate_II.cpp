b30de182e9729de7420d2665afdcd3258ea63028
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-01 01:22:16 +0530

    Problem No.: 219. Contains Duplicate II Using Both Map & Sliding Window Technique

--- Arrays/Contains_Duplicate_II.cpp
+++ Arrays/Contains_Duplicate_II.cpp
@@ -3,6 +3,7 @@
 /*
     Company Tags                : Airbnb, Palantir Technologies
     Leetcode Link               : https://leetcode.com/problems/contains-duplicate-ii/
+    Using sliding window        : https://github.com/mrunaltarwatkar018/Placement_Data_Structures_N_Algorithms/blob/master/Sliding%20Window/Contains_Duplicate_II_.cpp
 */
 
 // Approach 1: Using Map without sliding window
@@ -24,6 +25,37 @@ public:
         return false;
     }
 };
+
+
+// Approach 2: Using Sliding Window Technique
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int> st;
+
+        int i = 0, j = 0;
+
+        while ( j < n) {
+            // step 1
+            if (abs(i-j) > k) { //abs(i-j) <= k
+                st.erase(nums[i]);
+                i++; // shrink
+            }
+
+            // step 2: past me dekha hai nums[j] ?
+            if ( st.find(nums[j]) != st.end()) {
+                return true;
+            }
+
+            st.insert(nums[j]);
+            j++;
+        }
+        return false;
+    }
+};
+
+
 
 
 /*
--- Sliding Window/Contains_Duplicate_II_.cpp
+++ Sliding Window/Contains_Duplicate_II_.cpp
@@ -1,9 +1,30 @@
 /* Leetcode Problem No.: 219. Contains Duplicate II  */
 
 /*
-    Company Tags                : Airbnb, Palantir Technologies
-    Leetcode Link               : https://leetcode.com/problems/contains-duplicate-ii/
+    Company Tags                        : Airbnb, Palantir Technologies
+    Leetcode Link                       : https://leetcode.com/problems/contains-duplicate-ii/
+    Using Map (without sliding window)  : https://github.com/mrunaltarwatkar018/Placement_Data_Structures_N_Algorithms/blob/master/Arrays/Contains_Duplicate_II.cpp
 */
+
+// Approach 1: Using Map without sliding window
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i<nums.size(); i++) {
+            
+            if(mp.find(nums[i]) != mp.end() &&  //check if present in map and abs(i-j) <= k
+                abs(mp[nums[i]] - i) <= k)
+                return true;
+            else
+                mp[nums[i]] = i; //{number, index}
+            
+        }
+        
+        return false;
+    }
+};
 
 // Approach 2: Using Sliding Window Technique
 class Solution {
@@ -32,6 +53,8 @@ public:
         return false;
     }
 };
+
+
 
 
 /*
