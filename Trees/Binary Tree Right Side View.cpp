4597fcdbea955a49fa0cd860c7e229576aad5592
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-03 14:54:16 +0530

    Problem No.: 199. Binary Tree Right Side View using DFS

--- Trees/Binary Tree Right Side View.cpp
+++ Trees/Binary Tree Right Side View.cpp
@@ -45,14 +45,32 @@ public:
 
 
 
+// Approach-2 : Pre Order Traversal i.e., DFS
 
 
+class Solution {
+public:
 
+    void preOrder(TreeNode* root, int level, vector<int> & result) {
+        if (root == NULL) 
+            return;
 
+        if (result.size() < level) 
+            result.push_back(root -> val);
 
+        preOrder(root -> right, level + 1, result);
+        preOrder(root -> left, level + 1, result);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        if (root == NULL) 
+            return {};
+        vector <int> result;
 
+        preOrder(root, 1, result);
 
-
+        return result;
+    }
+};
 
 
 
