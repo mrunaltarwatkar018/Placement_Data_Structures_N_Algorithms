c910acb1ae4401a959f83a640415aa02fbc1078e
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-06 00:25:39 +0530

    Problem No.: 623. Add One Row to Tree

--- Trees/Add One Row to Tree.cpp
+++ Trees/Add One Row to Tree.cpp
@@ -142,9 +142,9 @@ public:
 //S.C : O(n)
 class Solution {
 public:
-    TreeNode* addOneRow(TreeNode* root, int v, int d) {
-        if(d == 1) {
-            TreeNode* newRoot = new TreeNode(v);
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* newRoot = new TreeNode(val);
             newRoot->left = root;
             return newRoot;
         }
@@ -161,9 +161,9 @@ public:
                 que.pop();
                 TreeNode* tempL = curr->left;
                 TreeNode* tempR = curr->right;
-                if(level == d-1) {
-                    curr->left  = new TreeNode(v);
-                    curr->right = new TreeNode(v);
+                if(level == depth-1) {
+                    curr->left  = new TreeNode(val);
+                    curr->right = new TreeNode(val);
                     curr->left->left   = tempL;
                     curr->right->right = tempR;
                     rowAdded = true;
@@ -180,6 +180,49 @@ public:
         return root;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     TreeNode* addOneRow(TreeNode* root, int v, int d) {
+//         if(d == 1) {
+//             TreeNode* newRoot = new TreeNode(v);
+//             newRoot->left = root;
+//             return newRoot;
+//         }
+//         queue<TreeNode*> que;
+        
+//         que.push(root);
+//         int level = 0;
+//         bool rowAdded = false;
+//         while(!que.empty()) {
+//             int n = que.size();
+//             level++;
+//             while(n--) {
+//                 TreeNode* curr = que.front();
+//                 que.pop();
+//                 TreeNode* tempL = curr->left;
+//                 TreeNode* tempR = curr->right;
+//                 if(level == d-1) {
+//                     curr->left  = new TreeNode(v);
+//                     curr->right = new TreeNode(v);
+//                     curr->left->left   = tempL;
+//                     curr->right->right = tempR;
+//                     rowAdded = true;
+//                 }
+//                 if(tempL)
+//                     que.push(tempL);
+//                 if(tempR)
+//                     que.push(tempR);
+//             }
+//             if(rowAdded)
+//                 break;
+//         }
+        
+//         return root;
+//     }
+// };
 
 
 /*
