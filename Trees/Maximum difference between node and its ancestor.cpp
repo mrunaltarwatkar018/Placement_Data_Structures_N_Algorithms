79937f0b780179813e83246fd021efee1be17004
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-06 17:26:20 +0530

    Problem No.: 1026. Maximum difference between node and its ancestor

---
+++ Trees/Maximum difference between node and its ancestor.cpp
@@ -0,0 +1,309 @@
+/*    Leetcode Problem No.: 1026. Maximum difference between node and its ancestor    */
+
+/*
+    Company Tags                : Google, Amazon, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/maximum-difference-between-node-and-its-ancestor/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+*/
+
+/************************************************************ C++ ***************************************************************************/
+// Approach-1 (Brute Force) - O(n^2) - n = number of nodes in the Tree
+/*
+    Just simply take a root, find all the differences of it from its childres and find max one
+    Again go to root->left and do the same as above
+    Again go to root->right and do the same as above
+*/
+
+
+class Solution {
+public:
+
+    int maxDiff;
+
+    void findMaxDiffUtil(TreeNode * root, TreeNode * child) {
+        if (root == NULL || child == NULL) return;
+
+        maxDiff = max(maxDiff, abs(root -> val - child -> val ));
+
+        findMaxDiffUtil(root, child -> left);
+        findMaxDiffUtil(root, child -> right);
+    }
+
+    void findMaxDiff (TreeNode * root) {
+
+        if(root == NULL) return;
+
+        findMaxDiffUtil(root, root -> left);
+        findMaxDiffUtil(root, root -> right);
+
+        findMaxDiff(root -> left);
+        findMaxDiff(root -> right);
+
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        maxDiff = -1;
+
+        findMaxDiff(root);
+
+        return maxDiff;
+    }
+};
+
+
+
+// another way
+class Solution
+{
+public:
+    int maxDiff;
+
+    void findMaxUtil(TreeNode *root, TreeNode *child)
+    {
+        if (!root || !child)
+            return;
+
+        maxDiff = max(maxDiff, abs(root->val - child->val));
+
+        findMaxUtil(root, child->left);
+        findMaxUtil(root, child->right);
+    }
+
+    void findMaxDiff(TreeNode *root)
+    {
+        if (!root || !root->left && !root->right)
+            return;
+
+        // Find max differences of this root with all its children
+        findMaxUtil(root, root->left);
+        findMaxUtil(root, root->right);
+
+        // firther move left and right
+        findMaxDiff(root->left);
+        findMaxDiff(root->right);
+    }
+
+    int maxAncestorDiff(TreeNode *root)
+    {
+        maxDiff = INT_MIN;
+
+        findMaxDiff(root);
+
+        return maxDiff;
+    }
+};
+
+
+
+
+
+
+
+
+
+// Approach-2
+// Optimal (O(n)) using min and max value for |min-max|
+
+/*
+    Approach Summary (Optimal Approach) - 
+        The key function is maxAncestorDiff, which initializes minimum and 
+        maximum values and then calls the recursive function findMaxDiff to 
+        compute the maximum difference between any two nodes in the binary tree.
+        
+        The findMaxDiff function traverses the tree recursively, updating the 
+        minimum and maximum values encountered along the way, and computes the 
+        maximum difference for each subtree. 
+
+        The final result is the maximum difference obtained across all subtrees, 
+        and it represents the maximum absolute difference between any two nodes 
+        in the given binary tree.
+*/
+
+class Solution
+{
+public:
+    int findMaxDiff(TreeNode *root, int minV, int maxV)
+    {
+        if (!root)
+            return abs(minV - maxV);
+
+        minV = min(root->val, minV);
+        maxV = max(root->val, maxV);
+
+        int l = findMaxDiff(root->left, minV, maxV);
+        int r = findMaxDiff(root->right, minV, maxV);
+
+        return max(l, r);
+    }
+
+    int maxAncestorDiff(TreeNode *root)
+    {
+        int minV = root->val;
+        int maxV = root->val;
+
+        return findMaxDiff(root, minV, maxV);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************************************************************ JAVA ***************************************************************************/
+/************************************************************  ***************************************************************************/
+// Approach-1 (Brute Force) - O(n^2) - n = number of nodes in the Tree
+/*
+    Just simply take a root, find all the differences of it from its childres and find max one
+    Again go to root->left and do the same as above
+    Again go to root->right and do the same as above
+*/
+class Solution
+{
+    int maxDiff;
+
+    void findMaxUtil(TreeNode root, TreeNode child)
+    {
+        if (root == null || child == null)
+        {
+            return;
+        }
+
+        maxDiff = Math.max(maxDiff, Math.abs(root.val - child.val));
+
+        findMaxUtil(root, child.left);
+        findMaxUtil(root, child.right);
+    }
+
+    void findMaxDiff(TreeNode root)
+    {
+        if (root == null || (root.left == null && root.right == null))
+        {
+            return;
+        }
+
+        // Find max differences of this root with all its children
+        findMaxUtil(root, root.left);
+        findMaxUtil(root, root.right);
+
+        // Further move left and right
+        findMaxDiff(root.left);
+        findMaxDiff(root.right);
+    }
+
+public
+    int maxAncestorDiff(TreeNode root)
+    {
+        maxDiff = Integer.MIN_VALUE;
+
+        findMaxDiff(root);
+
+        return maxDiff;
+    }
+}
+
+// Approach-2
+// Optimal (O(n)) using min and max value for |min-max|
+class Solution
+{
+public
+    int findMaxDiff(TreeNode root, int minV, int maxV)
+    {
+        if (root == null)
+        {
+            return Math.abs(minV - maxV);
+        }
+
+        minV = Math.min(root.val, minV);
+        maxV = Math.max(root.val, maxV);
+
+        int l = findMaxDiff(root.left, minV, maxV);
+        int r = findMaxDiff(root.right, minV, maxV);
+
+        return Math.max(l, r);
+    }
+
+public
+    int maxAncestorDiff(TreeNode root)
+    {
+        int minV = root.val;
+        int maxV = root.val;
+
+        return findMaxDiff(root, minV, maxV);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=cvekQzmBdcs
+*/
