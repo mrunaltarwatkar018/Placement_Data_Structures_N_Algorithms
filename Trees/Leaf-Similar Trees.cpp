1f7a7165c9f018e68fcd5cfb2fd905bcf290ce80
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-06 15:52:12 +0530

    Problem No.: 872. Leaf-Similar Trees

--- Trees/Leaf-Similar Trees.cpp
+++ Trees/Leaf-Similar Trees.cpp
@@ -55,7 +55,7 @@ public:
 
 
 
-
+// anpther way
 class Solution {
 public:
 
@@ -90,7 +90,41 @@ public:
 };
 
 
+// using vector
+//T.C : O(n)
+//S.C : O(n)
 
+class Solution {
+private:
+
+    void addLeafToSum(TreeNode* root, vector<int>& ourVec) {
+        if (!root) {
+            return;
+			// Push back the value of each leaf into our vector
+        } else if (root && root->left == nullptr && root->right == nullptr) {
+            ourVec.push_back(root->val);
+        }
+        addLeafToSum(root->left, ourVec);
+        addLeafToSum(root->right, ourVec);
+        return;
+    }
+    
+public:
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> root1Vec;
+        vector<int> root2Vec;
+        addLeafToSum(root1, root1Vec);
+        addLeafToSum(root2, root2Vec);
+		// If there are a different number of leafs, it can't be the same
+        if (root1Vec.size() != root2Vec.size()) return false;
+        for (int i{}; i < root1Vec.size(); ++i) {
+            if (root1Vec[i] != root2Vec[i]) return false;
+        }
+		// If all checks passed, return true
+        return true;
+    }
+};
 
 
 
