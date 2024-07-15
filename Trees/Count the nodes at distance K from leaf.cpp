f1e999be6f9bbcb7049ef871fd59f03d15c9a999
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-15 19:58:20 +0530

    GFG Problem: Count the nodes at distance K from leaf

--- Trees/Count the nodes at distance K from leaf.cpp
+++ Trees/Count the nodes at distance K from leaf.cpp
@@ -64,6 +64,68 @@ class Solution {
 };
 
 
+
+// another way
+class Solution
+{
+    public:
+    
+    vector<vector<Node*>> paths;
+    
+    void solve(Node * root, vector<Node *> & temp) {
+        if (!root)
+            return;
+            
+        temp.push_back(root);
+        
+        if ( !root -> left && !root -> right) {
+            paths.push_back(temp);
+        }
+        
+        solve(root -> left, temp);
+        solve(root -> right, temp);
+        
+        temp.pop_back();
+    }
+    
+    
+    //Function to return count of nodes at a given distance from leaf nodes.
+        int printKDistantfromLeaf(Node* root, int k)
+        {
+            vector<Node *> temp;
+            solve(root, temp);
+            
+            unordered_set<Node *> result;
+            
+            for ( vector<Node *> &path : paths) {
+                int n = path.size();
+                
+                if ( n > k ) {
+                    result.insert(path[n-k-1]);
+                }
+            }
+            return result.size();
+        }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Approach-2 (Without storing all root-leaf paths)
 //T.C : O(n)
 //S.C : O(h)
