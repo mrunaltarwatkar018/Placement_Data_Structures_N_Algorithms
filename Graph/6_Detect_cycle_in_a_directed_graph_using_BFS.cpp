afd0bfc0ffa4ff6cab0a5757102cca4de4a92a04
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-17 18:45:49 +0530

    GFG problem: Directed Graph Cycle : Using Khan's Alogorithm (BFS)

--- Graph/6_Detect_cycle_in_a_directed_graph_using_BFS.cpp
+++ Graph/6_Detect_cycle_in_a_directed_graph_using_BFS.cpp
@@ -6,8 +6,67 @@
 */
 
 
+class Solution {
+    public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> que;
+        
+        vector<int> indegree(N, 0);
+        int count = 0;
+            
+        //1. 
+        for ( int  u = 0; u < N; u++ ) {
+            for ( int &v : adj[u] ) {
+                indegree[v]++;
+            } 
+        }
+        
+        //2. Fill que, indegree with 0
+        for ( int i = 0; i < N; i++ ) {
+            if (indegree[i] == 0) {
+                que.push(i);
+                count++;
+            }
+        }
+        
+        //3. Simple BFS
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            
+            for ( int & v : adj[u]) {
+                indegree[v]--;
+                
+                if ( indegree[v] == 0) {
+                    que.push(v);
+                    count++;
+                }
+            }
+        }
+        
+        
+        if (count == N) { // we visited all nodes, i.e., no cycle is present in a graph
+            return false;
+        } else { // we visited all nodes, i.e., cycle is present in a graph
+            return true;
+        }
+
+    }
+};
 
 
+
+
+
+
+
+
+
+
+
+
+// or can be way as
 
 
     class Solution {
@@ -17,6 +76,7 @@
             queue<int> que;
             vector<int> indegree(N, 0);
             int count = 0;
+
             //1
             for(int u = 0; u<N; u++) {
                 for(int &v : adj[u]) {
