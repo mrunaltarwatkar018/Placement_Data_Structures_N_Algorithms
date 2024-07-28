f95850219e39c058d5ea30017c4e5260e9f4d4a0
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-07-28 23:32:05 +0530

    Problem No.: 1431. Kids With the Greatest Number of Candies

---
+++ Arrays/Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,63 @@
+/* Leetcode Problem No.: 1431. Kids With the Greatest Number of Candies  */
+
+
+/*
+    Company Tags                : NA
+    Leetcode Link               : https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+*/
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        
+        int maxC = *max_element(begin(candies), end(candies));
+        
+        vector<bool> result(n, false);
+        
+        for(int i = 0; i<n; i++) {
+            
+            if(candies[i] + extraCandies >= maxC) {
+                result[i] = true;
+            }
+            
+        }
+        return result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=ZUEKt3TAYLc
+*/
