e930696cd2578889ab715d6efa1e2e14acbd380e
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-08-08 20:52:26 +0530

    GFG Problem No.: Sum Tree

---
+++ Trees/Sum Tree.cpp
@@ -0,0 +1,38 @@
+/* GFG Problem No.: Sum Tree   */
+
+/*
+    Company Tags                : Amazon, Microsoft, D-E-Shaw, Goldman Sachsm Adobe
+    GFG Link                    : https://www.geeksforgeeks.org/problems/sum-tree/1
+*/
+
+class Solution {
+public:
+    int traverse(Node* root){
+        if(root==NULL) return 0;
+        int left=traverse(root->left);
+        int right=traverse(root->right);
+        return root->data + left +right;
+        
+    }
+    bool isSumTree(Node* root) {
+        
+        if(root==NULL) return true;
+        if(root->left==NULL && root->right==NULL) return true;
+        if(root->left != NULL || root->right != NULL){
+            if(root->data != traverse(root->left) + traverse(root->right)){
+                return false;
+            }
+        }
+        return isSumTree(root->left) && isSumTree(root->right);
+    }
+};
+
+
+
+
+
+
+
+/*
+    YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=4MZ1_2mgu90
+*/
