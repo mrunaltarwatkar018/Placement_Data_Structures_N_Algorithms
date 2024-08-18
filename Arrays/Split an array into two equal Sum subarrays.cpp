d4916c3c105b3d82e86f6b4944072f6e0d405006
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-08-18 14:14:22 +0530

    GFG Problem: Split an array into two equal Sum subarrays

--- Arrays/Split an array into two equal Sum subarrays.cpp
+++ Arrays/Split an array into two equal Sum subarrays.cpp
@@ -99,3 +99,33 @@ class Solution:
             if right_sum == left_sum:
                 return True
         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript
+class Solution {
+    canSplit(arr) {
+        let leftSum = arr.reduce((acc, val) => acc + val, 0);
+        let rightSum = 0;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            rightSum += arr[i];
+            leftSum -= arr[i];
+            if (rightSum === leftSum) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
