cfa75a68cd4eb830be8baa4127fc546ca2c2c3d6
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-08-18 14:11:48 +0530

    GFG Problem: Split an array into two equal Sum subarrays

--- Arrays/Split an array into two equal Sum subarrays.cpp
+++ Arrays/Split an array into two equal Sum subarrays.cpp
@@ -22,3 +22,26 @@ class Solution {
         return false;
     }
 };
+
+
+
+// another way
+class Solution {
+    public:
+    bool canSplit(vector<int>& arr) {
+        int leftSum = 0;
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+            leftSum += arr[i];
+
+        int rightSum = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            rightSum += arr[i];
+            leftSum -= arr[i];
+            if (rightSum == leftSum)
+                return true;
+        }
+        return false;
+    }
+};
