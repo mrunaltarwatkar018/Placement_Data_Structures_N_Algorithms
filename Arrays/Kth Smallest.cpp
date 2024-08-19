ceca0bbcd80c15d282ce4a676acd9381e08ae6ea
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-08-19 12:35:38 +0530

    GFG Problem: Kth Smallest

--- Arrays/Kth Smallest.cpp
+++ Arrays/Kth Smallest.cpp
@@ -23,3 +23,25 @@ class Solution {
         return 0;
     }
 };
+
+
+
+
+// Java Solution
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> set = new TreeSet<>(mp.keySet());
+        Iterator<Integer> it = set.iterator();
+        while(it.hasNext()){
+            k--;
+            if(k==0) return it.next();
+            it.next();
+        }
+        return 0;
+    }
+}
