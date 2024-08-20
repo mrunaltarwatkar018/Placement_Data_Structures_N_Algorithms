7c554e504c236dcddd1d1d96fbd614f239f818bc
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-08-20 15:38:30 +0530

    GFG Problem: Burning Tree

---
+++ Trees/Burning Tree.cpp
@@ -0,0 +1,62 @@
+/* GFG Problem: Burning Tree */
+
+/*
+    Company Tags                : Flipkart, Amazon, Microsoft, Adobe
+    GFG Link                    : https://www.geeksforgeeks.org/problems/burning-tree/1
+*/
+
+
+
+// python code
+
+/*
+    Time and Space Complexity
+        - Time Complexity: The algorithm runs a BFS, which has a time complexity of O(N), where N is the number of nodes in the binary tree.
+        - Space Complexity: The space complexity is O(N) as we use a parent map and a queue to perform BFS.
+*/
+
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.parent_map = {}  # A dictionary to keep track of parent nodes
+        self.target_node = None  # The target node we are searching for
+        self.min_time = 0  # The minimum time to reach the target node
+
+    def buildParentMap(self, node, target):
+        """
+        Build a parent map while searching for the target node.
+        """
+        if node.data == target:
+            self.target_node = node
+        if node.left:
+            self.parent_map[node.left] = node
+            self.buildParentMap(node.left, target)
+        if node.right:
+            self.parent_map[node.right] = node
+            self.buildParentMap(node.right, target)
+
+    def minTime(self, root, target):
+        """
+        Calculate the minimum time to reach the target node from the root.
+        """
+        self.buildParentMap(root, target)
+
+        queue = deque()
+        queue.append((self.target_node, 0))
+        visited = set()
+
+        while queue:
+            current_node, time = queue.popleft()
+            visited.add(current_node)
+
+            if current_node in self.parent_map and self.parent_map[current_node] not in visited:
+                queue.append((self.parent_map[current_node], time + 1))
+            if current_node.left and current_node.left not in visited:
+                queue.append((current_node.left, time + 1))
+            if current_node.right and current_node.right not in visited:
+                queue.append((current_node.right, time + 1))
+
+            self.min_time = time  # Update the minimum time
+
+        return self.min_time
