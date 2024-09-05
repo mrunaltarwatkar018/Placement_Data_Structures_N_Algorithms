556e25139ceb0da6ce0befc1b844aef28daf6b32
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-09-05 19:31:33 +0530

    GFG Problem: Missing in Array

---
+++ Arrays/Leetcode Easy/Missing in Array.cpp
@@ -0,0 +1,35 @@
+/* GFG Problem: Missing in Array */
+
+/*
+    Company Tags                : Flipkart, Morgan Stanley, Accolite, Amazon, Microsoft, D-E-Shaw, Ola Cabs, Payu, Visa, Intuit, Adobe, Cisco, Qualcommm TCS
+    GFG Link                    : https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1
+*/
+
+
+
+
+
+// C++ code  T.C =O(N) 
+
+
+
+
+
+
+
+
+// C++ code  T.C =O(N) 
+
+class Solution {
+    public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+        long long sum = 0;
+        for(auto &e : arr){
+            sum += (long long)e;
+        }
+        long long desiredSum = (long long)n*(n + 1)/2LL;
+        return desiredSum - sum;
+    }
+};
