ffaf012e52ed026fd1cea96c136d0926f2ab79e8
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-09-16 18:48:30 +0530

    GFG Problem No.: Shortest Path in Weighted undirected graph

--- Graph/Dijkstra's Based Problems/3_Shortest_Path_in_Weighted_undirected_graph.cpp
+++ Graph/Dijkstra's Based Problems/3_Shortest_Path_in_Weighted_undirected_graph.cpp
@@ -69,3 +69,89 @@ class Solution {
 		return path;
     }
 };
+
+
+
+// another way
+
+vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        
+        int source = 1;
+
+        // constructing the adjacency list
+        unordered_map<int, vector<pair<int, int>>> adj;
+    
+        for(auto vec: edges){
+            auto u = vec[0];
+            auto v = vec[1];
+            auto wt = vec[2];
+            
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+    
+        // priority queue to store {distance, node}
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    
+        // distances from source to each node(ndexes)
+        vector<int> result(n + 1, INT_MAX);
+    
+        // parent of each node for path reconstruction
+        vector<int> parent(n + 1, -1);
+    
+        // initialize parent vector
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+    
+        // source to source distance
+        result[source] = 0;
+        pq.push({0, source}); // {distance, node}
+    
+        while (!pq.empty()) {
+            pair<int, int> top = pq.top();
+            pq.pop();
+            
+            int dist = top.first;
+            int u = top.second;
+    
+            // visit adjacent nodes
+            for (auto &v : adj[u]) {
+                int adjNode = v.first;
+                int adjDist = v.second;
+    
+                if (adjDist + dist < result[adjNode]) {
+                    result[adjNode] = adjDist + dist;
+                    pq.push({adjDist + dist, adjNode});
+    
+                    // update parent of adjNode
+                    parent[adjNode] = u;
+                }
+            }
+        }
+    
+        // edge case: if destination is unreachable
+        int destNode = n;
+        if (result[destNode] == INT_MAX) return {-1};
+    
+        // reconstruct path from destination to source using parent array
+        vector<int> path;
+    
+        while (parent[destNode] != destNode) {
+            path.push_back(destNode);
+            destNode = parent[destNode];
+        }
+    
+        // add the source node to the path
+        path.push_back(source);
+        
+        // pushing the max dist as req in the problem statement
+        //restlt vec will store -> dist from source to i -> 
+        //at idx n -> dist from source to n-> which is our req max dist
+        path.push_back(result[n]);
+        
+        // reverse to get the path from source to destination
+        reverse(path.begin(), path.end());
+    
+        return path;
+    }
