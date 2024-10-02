31c507709f2e079494476eb81a88e6b190eef6c2
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-10-02 15:31:30 +0530

    Problem No.: 1331. Rank Transform of an Array

---
+++ Arrays/Rank Transform of an Array.cpp
@@ -0,0 +1,31 @@
+/* Leetcode Problem No.: 1331. Rank Transform of an Array */
+
+/*
+    Company Tags                 : 
+    Leetcode Qn Link             : https://leetcode.com/problems/rank-transform-of-an-array/description/
+*/
+
+
+
+//T.C : O(n*logn)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        unordered_map<int, int> mp; //number -> rank
+        set<int> st(begin(arr), end(arr)); //ordered set keeps them in sorted order
+
+        int rank = 1;
+        for(auto &num : st) {
+            mp[num] = rank;
+            rank++;
+        }
+
+        for(int i = 0; i < arr.size(); i++) {
+            arr[i] = mp[arr[i]];
+        }
+
+        return arr;
+
+    }
+};
