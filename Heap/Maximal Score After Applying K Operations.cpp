675e8f56322d11105a15e04e188fddbcc2644456
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-10-14 14:42:23 +0530

    Problem No.: 2530. Maximal Score After Applying K Operations

---
+++ Heap/Maximal Score After Applying K Operations.cpp
@@ -0,0 +1,65 @@
+/* Leetcode Problem No.: 2530. Maximal Score After Applying K Operations  */
+
+/*
+    Company Tags                : 
+    Leetcode Qn Link            : https://leetcode.com/problems/maximal-score-after-applying-k-operations
+*/
+
+
+
+/************************************************************ C++ ************************************************************/
+//Approach (standard heap approach)
+//T.C : O(n + klogn)
+//S.C : O(k)
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        long long sum = 0;
+
+        priority_queue<int> pq(begin(nums), end(nums)); //First time creation of heap "heapify" is O(n)
+        //Refer - https://stackoverflow.com/questions/9755721/how-can-building-a-heap-be-on-time-complexity
+        //Or you can refer NPTEL video on Heap on youtube. 
+
+        while(k--) {
+            int maxEl = pq.top();
+            pq.pop();
+
+            sum += maxEl;
+            maxEl = ceil(maxEl/3.0);
+            pq.push(maxEl);
+        }
+
+        return sum;
+    }
+};
+
+
+/************************************************************ JAVA ************************************************************/
+//Approach (standard heap approach)
+//T.C : O(nlogn + klogn)
+//S.C : O(k)
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        long sum = 0;
+        
+        // Use a max-heap (priority queue with reverse order)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //Java DOESN't provide heapify in O(n)
+        for (int num : nums) {
+            pq.add(num);
+        }
+
+        while (k-- > 0) {
+            int maxEl = pq.poll();
+            sum += maxEl;
+
+            // Add back the updated element
+            maxEl = (int) Math.ceil(maxEl / 3.0);
+            pq.add(maxEl);
+        }
+
+        return sum;
+    }
+}
