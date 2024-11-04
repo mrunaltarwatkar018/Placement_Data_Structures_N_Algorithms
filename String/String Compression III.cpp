67c46ad934301ea093183af450aeaa8cda143ca8
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-11-04 15:50:01 +0530

    Problem No.: 3136. String Compression III

---
+++ String/String Compression III.cpp
@@ -0,0 +1,63 @@
+/* Leetcode Problem No.: 3136. String Compression III  */
+
+/*
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/string-compression-iii
+*/
+
+
+/**************************************************************** C++ ****************************************************************/
+//Approach (Simple Simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+
+        int n = word.length();
+        int i = 0;
+
+        while(i < n) {
+            int count = 0;
+            char ch   = word[i];
+
+            while(i < n && count < 9 && word[i] == ch) {
+                count++;
+                i++;
+            }
+
+            comp += to_string(count) + ch;
+        }
+
+        return comp;
+    }
+};
+
+
+
+/**************************************************************** JAVA ****************************************************************/
+//Approach (Simple Simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+        int n = word.length();
+        int i = 0;
+
+        while (i < n) {
+            int count = 0;
+            char ch = word.charAt(i);
+
+            while (i < n && count < 9 && word.charAt(i) == ch) {
+                count++;
+                i++;
+            }
+
+            comp.append(count).append(ch);
+        }
+
+        return comp.toString();
+    }
+}
