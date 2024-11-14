9c9ad492d6bae9fc3b7bf1c98334cab6ca3fe951
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-11-14 14:41:05 +0530

    Problem No.: 2064. Minimized Maximum of Products Distributed to Any Store

---
+++ Arrays/Binary Search/Minimized Maximum of Products Distributed to Any Store.cpp
@@ -0,0 +1,8 @@
+/*    Leetcode Problem No.: 2064. Minimized Maximum of Products Distributed to Any Store  */
+
+/*
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/minimized-maximum-of-products-distributed-to-any-store/
+*/
+
+
