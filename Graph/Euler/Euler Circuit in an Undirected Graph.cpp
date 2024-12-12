fc76dfe7691a7744ed449b5bec8dfc1b8cc6a9ce
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-13 01:33:36 +0530

    GFG Problem: Euler Circuit in an Undirected Graph

---
+++ Graph/Euler/Euler Circuit in an Undirected Graph.cpp
@@ -0,0 +1,54 @@
+/* GFG Problem: Euler Circuit in an Undirected Graph  */
+
+/*
+    Company Tags                : Will update soon.
+    GfG Link                    : https://www.geeksforgeeks.org/problems/euler-circuit-in-a-directed-graph/1
+*/
+
+
+/******************************************************** C++ ****************************************************************/
+//T.C : O(V)
+//S.C : O(1)
+class Solution {
+public:
+	bool isEularCircuitExist(int V, vector<int>adj[]){
+	    vector<int> degree(V, 0);
+	    
+	    for(int i = 0; i<V; i++) {
+	        
+	        degree[i] = adj[i].size();
+	        
+	    }
+	    
+	    for(int i = 0; i<V; i++) {
+	        if(degree[i] % 2 != 0) {
+	            return false;
+	        }
+	    }
+	    
+	    return true;
+	}
+};
+
+
+
+/******************************************************** JAVA ****************************************************************/
+//T.C : O(V)
+//S.C : O(1)
+public class Solution {
+    public boolean isEularCircuitExist(int V, List<Integer>[] adj) {
+        int[] degree = new int[V];
+
+        for (int i = 0; i < V; i++) {
+            degree[i] = adj[i].size();
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (degree[i] % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
