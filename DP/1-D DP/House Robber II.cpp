0266aaac17cb048f18386da6119d32fd1c46f2f6
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-26 02:40:19 +0530

    Problem No.: 213. House Robber II

--- DP/1-D DP/House Robber II.cpp
+++ DP/1-D DP/House Robber II.cpp
@@ -92,3 +92,79 @@ public:
 
 
 
+//Approach-2 (Using Bottom Up similar to House Robber-1 and just taking two cases
+/*
+    Case-1 (Take from 1st House - Hence skip the last house)
+    Case-2 (Take from 2nd House - Hence take the last house)
+*/
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        vector<int> t(n+1, 0);
+        //t[i] = money stolen when you have i houses
+        t[0] = 0;
+
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        for (int i = 1; i <= n - 1; i++) {
+            int skip = t[i-1];
+            int stolen = nums[i-1] + ( (i - 2 >= 0) ? t[i-2] : 0);
+
+            t[i] = max(skip, stolen);
+        }
+        int result1 = t[n-1];
+
+        t.clear();
+
+        //Case-2 (skip 1st House - Hence you can take the last house)
+        t[0] = 0;
+        t[1] = 0;  // skip 1st house
+        for (int i = 2; i <= n; i++) {
+            int skip = t[i-1];
+            int stolen = nums[i-1] + ( (i - 2 >= 0) ? t[i-2] : 0);
+            t[i] = max(skip, stolen);
+        }
+        int result2 = t[n];
+
+        return max(result1, result2);
+    }
+};
+
+// or
+class Solution {
+public:
+    int n;
+    
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        if(n == 1)
+            return nums[0];
+        
+        vector<int> t(n+1, 0);
+        //t[i] = Max money gained from i houses
+        
+        int result1 = 0;
+        int result2 = 0;
+        
+        //Case-1 (Take from 1st House - Hence skip the last house)
+        t[0] = 0;
+        for(int i = 1; i<=n-1; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result1 = t[n-1];
+        
+        t.clear();
+        //Case-2 (Take from 2nd House - Hence take the last house)
+        t[0] = 0;
+        t[1] = 0;
+        for(int i = 2; i<=n; i++) {
+            t[i] = max(t[i-1] , nums[i-1]+ ((i-2 >= 0) ? t[i-2] : 0 ));
+        }
+        result2 = t[n];
+        
+        return max(result1, result2);
+        
+    }
+};
