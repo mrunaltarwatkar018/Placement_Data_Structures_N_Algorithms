17d7cc080767f1b65226dab342a8046ebc461ec0
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-26 18:34:54 +0530

    Problem No.: 1911. Maximum Alternating Subsequence Sum

--- DP/1-D DP/05 Maximum Alternating Subsequence Sum.cpp
+++ DP/1-D DP/05 Maximum Alternating Subsequence Sum.cpp
@@ -154,3 +154,23 @@ public:
     }
 };
 
+//Approach-4 (Same as Approach-3 - Only difference is that I have used 1 based indexing to make it general to other Botto Up)
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<long>> t(n+1, vector<long>(2, 0));
+            
+        for(int i = 1; i < n+1; i++) {
+            
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+            
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+            
+            
+        }
+        
+        return max(t[n][0], t[n][1]);
+    }
+};
