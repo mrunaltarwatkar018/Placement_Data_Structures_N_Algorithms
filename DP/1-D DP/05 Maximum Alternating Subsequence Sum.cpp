ed84b9aa875c01a46626e6879a330e8bd9270232
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-26 18:27:07 +0530

    Problem No.: 1911. Maximum Alternating Subsequence Sum

--- DP/1-D DP/05 Maximum Alternating Subsequence Sum.cpp
+++ DP/1-D DP/05 Maximum Alternating Subsequence Sum.cpp
@@ -108,3 +108,49 @@ public:
     }
 };
 
+
+//Approach-3 (Bottom Up) -- T.C : O(n), space : O(n)
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<long>> t(n+1, vector<long>(2, 0)); // even : 0 , odd : 1
+            
+        for(int i = 1; i < n + 1; i++) {
+            // even length subsequence
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+            
+            // odd length subsequence
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+            
+        }
+        
+        return max(t[n][0], t[n][1]);
+    }
+};
+
+// or
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<long>> t(n, vector<long>(2, 0));
+
+        t[0][0] = max(-nums[0], 0);
+        t[0][1] = max(nums[0], 0);
+            
+        for(int i = 1; i < n; i++) {
+            
+            t[i][0] = max(t[i-1][1] - nums[i], t[i-1][0]);
+            
+            t[i][1] = max(t[i-1][0] + nums[i], t[i-1][1]);
+            
+            
+        }
+        
+        return max(t[n-1][0], t[n-1][1]);
+    }
+};
+
