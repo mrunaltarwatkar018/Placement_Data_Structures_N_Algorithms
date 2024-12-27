8de7c3718f53ca618a967b1c9901bf791de2dd68
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-27 16:44:42 +0530

    Problem No.: 300. Longest Increasing Subsequence

--- DP/LIS & its Variants/01 Longest Increasing Subsequence.cpp
+++ DP/LIS & its Variants/01 Longest Increasing Subsequence.cpp
@@ -141,3 +141,51 @@ public:
     }
 };
 
+// Approacj-4 (Using concept of Patience Sorting (O(nlogn))
+// T.C : O(nlogn)
+// S.C : O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted; // empty in the beginning
+
+        for (int i = 0; i < n; i++ ) {
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]); // just greater that or equal the nums[i]
+            // and if do *it we get element
+            if (it == end(sorted)) {
+                sorted.push_back(nums[i]); // greatest : so insert it
+            } else {
+                *it = nums[i]; // replace with nums[i]
+            }
+        }
+        return sorted.size(); // LIS
+    }
+};
+
+// or
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+            /*
+                    Why lower bound ?
+                    We want increasing subsequence and  hence 
+                    we want to eliminate the duplicates as well.
+                    lower_bound returns iterator to "next greater or equal to"
+			*/
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); //greatest : so insert it
+            else
+                *it = nums[i]; //replace
+        }
+        
+        return (int)sorted.size();
+    }
+};
+
