15593dd67a73394bf23af54d127cfe0602b3f236
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2024-12-31 17:04:57 +0530

    LIS variant 4: Problem No.: 2926. Maximum Balanced Subsequence Sum

--- DP/LIS & its Variants/05 Maximum Balanced Subsequence Sum.cpp
+++ DP/LIS & its Variants/05 Maximum Balanced Subsequence Sum.cpp
@@ -200,6 +200,40 @@ class Solution {
 public:
     long long maxBalancedSubsequenceSum(vector<int>& nums) {
         int n = nums.size();
+        map<int, long long> mp;  // nums[i] - i
+
+        long long result = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            auto it = mp.upper_bound(nums[i]-i); // it -> first (nums[j] - j), it -> second : sum
+
+            long long sum = nums[i];
+            
+            if ( it != mp.begin()) {
+                it--;
+                // sum += *it.second; or
+                sum += it -> second;
+            }
+
+            mp[nums[i] - i] = max(mp[nums[i] - i], sum);
+
+            it = mp.upper_bound(nums[i] - i);
+
+            while(it != mp.end() && it -> second <= sum) {
+                mp.erase(it++);
+            }
+
+            result = max(result, sum);
+        }
+        return result;
+    }
+};
+
+// or
+class Solution {
+public:
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
+        int n = nums.size();
         map<int, long long> mp;
 
         long long ans = INT_MIN;
