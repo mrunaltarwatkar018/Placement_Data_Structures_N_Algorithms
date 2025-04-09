d28ba56f6076b91158867bee4924941eb745b16b
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-04-09 17:51:01 +0530

    Problem No.: 3375. Minimum Operations to Make Array Values Equal to K

---
+++ HashSet/Leetcode Easy/Minimum Operations to Make Array Values Equal to K.cpp
@@ -0,0 +1,48 @@
+/* Leetcode Problem No.: 3375. Minimum Operations to Make Array Values Equal to K  */
+
+/*
+    Company Tags                : 
+    Leetcode Link               : https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k
+*/
+
+
+/*************************************************************** C++ ***************************************************************/
+//Approach (Using observation - counting unique)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public:
+        int minOperations(vector<int>& nums, int k) {
+            unordered_set<int> st;
+    
+            for(int &x : nums) {
+                if(x < k) {
+                    return -1;
+                } else if(x > k) {
+                    st.insert(x);  
+                }
+            }
+            return st.size();
+        }
+    };
+    
+    
+    /*************************************************************** JAVA ***************************************************************/
+    //Approach (Using observation - counting unique)
+    //T.C : O(n)
+    //S.C : O(n)
+    class Solution {
+        public int minOperations(int[] nums, int k) {
+            Set<Integer> set = new HashSet<>();
+    
+            for (int x : nums) {
+                if (x < k) {
+                    return -1;
+                } else if (x > k) {
+                    set.add(x);
+                }
+            }
+    
+            return set.size();
+        }
+    }
