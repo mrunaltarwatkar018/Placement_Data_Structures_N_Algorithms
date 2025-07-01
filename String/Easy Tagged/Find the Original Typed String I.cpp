2115bfae6abe135226f68cb6485a8113b8fb584d
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-07-01 13:05:49 +0530

    Problem No.: 3330. Find the Original Typed String I

---
+++ String/Easy Tagged/Find the Original Typed String I.cpp
@@ -0,0 +1,44 @@
+/* Leetcode Problem No.: 3330. Find the Original Typed String I  */
+
+/*
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/find-the-original-typed-string-i
+*/
+
+/****************************************************************** C++ ******************************************************************/
+//Approach (straight forward traversal)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count = 0;
+
+        for(int i = 1; i < word.length(); i++) {
+            if(word[i] == word[i-1])
+                count++;
+        }
+
+        return count+1; //+1 for when Alice does no long press
+    }
+};
+
+
+
+/****************************************************************** JAVA ******************************************************************/
+//Approach (straight forward traversal)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int possibleStringCount(String word) {
+        int count = 0;
+
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                count++;
+            }
+        }
+
+        return count + 1; // +1 for the base case (no long presses)
+    }
+}
