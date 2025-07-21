4ac8d7ed3a40c6a8384adb7b6466369936461711
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-07-21 13:20:23 +0530

    Problem No.: 1957. Delete Characters to Make Fancy String

---
+++ String/Easy Tagged/Delete Characters to Make Fancy String.cpp
@@ -0,0 +1,66 @@
+/*    Leetcode Problem No.: 1957. Delete Characters to Make Fancy String    */
+
+/*
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/delete-characters-to-make-fancy-string
+*/
+
+/****************************************** C++ ***********************************************************/
+//Approach (Simple straight forward traversal)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int n = s.length();
+
+        string result = "";
+
+        result.push_back(s[0]);
+        int freq = 1;
+
+        for(int i = 1; i < n; i++) {
+            if(s[i] == result.back()) {
+                freq++;
+                if(freq < 3) {
+                    result.push_back(s[i]);
+                }
+            } else {
+                result.push_back(s[i]);
+                freq = 1;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+/***************************************************** JAVA ****************************************************/
+//Approach (Simple straight forward traversal)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public String makeFancyString(String s) {
+        int n = s.length();
+        StringBuilder result = new StringBuilder();
+
+        // Start with the first character
+        result.append(s.charAt(0));
+        int freq = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == result.charAt(result.length() - 1)) {
+                freq++;
+                if (freq < 3) {
+                    result.append(s.charAt(i));
+                }
+            } else {
+                result.append(s.charAt(i));
+                freq = 1;
+            }
+        }
+
+        return result.toString();
+    }
+}
