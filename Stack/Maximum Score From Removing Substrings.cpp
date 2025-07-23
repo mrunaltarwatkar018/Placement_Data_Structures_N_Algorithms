7a01729daa7a99f136f7591589c08d55057b1501
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-07-23 13:24:42 +0530

    Problem No.: 1717. Maximum Score From Removing Substrings

---
+++ Stack/Maximum Score From Removing Substrings.cpp
@@ -0,0 +1,208 @@
+/*    Leetcode Problem No.: 1717. Maximum Score From Removing Substrings    */
+
+/*
+    Company Tags                : Google, Meta, Amazon
+    Leetcode Link               : https://leetcode.com/problems/maximum-score-from-removing-substrings
+*/
+
+
+/**************************************************************** C++ ********************************************************/
+//Approach-1 (Using Stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (maxStr == "ab") ? "ba" : "ab"; //This is updated after the video was made as a new test case was added in Leetcode
+
+        //First Pass
+        string temp_first     = removeSubstring(s, maxStr);
+        int removedPairsCount = (n - temp_first.length()) / 2;
+        score                += removedPairsCount * max(x, y);
+
+
+        //Second Pass
+        string temp_second = removeSubstring(temp_first, minStr);
+        removedPairsCount  = (temp_first.length() - temp_second.length()) / 2;
+        score             += removedPairsCount * min(x, y);
+
+        return score;
+    }
+
+private:
+    string removeSubstring(string& s, string& matchStr) {
+        stack<char> st;
+
+        for (char &ch : s) {
+            if (ch == matchStr[1] && !st.empty() && st.top() == matchStr[0]) {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+
+        string remainStr;
+        while (!st.empty()) {
+            remainStr.push_back(st.top());
+            st.pop();
+        }
+        reverse(remainStr.begin(), remainStr.end());
+        return remainStr;
+    }
+};
+
+
+
+//Approach-2 (Without Stack)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (maxStr == "ab") ? "ba" : "ab"; //This is updated after the video was made as a new test case was added in Leetcode
+
+        //First Pass
+        string temp_first     = removeSubstring(s, maxStr);
+        int L                 = temp_first.length();
+        int removedPairsCount = (n - L) / 2;
+        score                += removedPairsCount * max(x, y);
+
+
+        //Second Pass
+        string temp_second = removeSubstring(temp_first, minStr);
+        removedPairsCount  = (L - temp_second.length()) / 2;
+        score             += removedPairsCount * min(x, y);
+
+        return score;
+    }
+
+    string removeSubstring(string& inputString, string& matchStr) {
+        int j = 0;
+
+        for (int i = 0; i < inputString.size(); i++) {
+            inputString[j++] = inputString[i];
+
+            if (j > 1 &&
+                inputString[j - 2] == matchStr[0] &&
+                inputString[j - 1] == matchStr[1]) {
+                j -= 2;
+            }
+        }
+
+        inputString.erase(inputString.begin() + j, inputString.end());
+
+        return inputString;
+    }
+};
+
+
+
+
+/**************************************************************** JAVA ***********************************************************/
+//Approach-1 (Using Stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+
+        String maxStr = (x > y) ? "ab" : "ba";
+        String minStr;
+        if(maxStr.equals("ab")) { //This is updated after the video was made as a new test case was added in Leetcode
+            minStr = "ba";
+        }
+        else {
+            minStr = "ab";
+        }
+
+        // First Pass
+        String tempFirst = removeSubstring(s, maxStr);
+        int removedPairsCount = (n - tempFirst.length()) / 2;
+        score += removedPairsCount * Math.max(x, y);
+
+        // Second Pass
+        String tempSecond = removeSubstring(tempFirst, minStr);
+        removedPairsCount = (tempFirst.length() - tempSecond.length()) / 2;
+        score += removedPairsCount * Math.min(x, y);
+
+        return score;
+    }
+
+    private String removeSubstring(String s, String matchStr) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && ch == matchStr.charAt(1) && stack.peek() == matchStr.charAt(0)) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder remainStr = new StringBuilder();
+        while (!stack.isEmpty()) {
+            remainStr.append(stack.pop());
+        }
+        return remainStr.reverse().toString();
+    }
+}
+
+
+
+//Approach-2 (Without Stack)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int n = s.length();
+        int score = 0;
+
+        String maxStr = (x > y) ? "ab" : "ba";
+        String minStr;
+        if(maxStr.equals("ab")) { //This is updated after the video was made as a new test case was added in Leetcode
+            minStr = "ba";
+        }
+        else {
+            minStr = "ab";
+        }
+
+        // First Pass
+        String tempFirst = removeSubstring(s, maxStr);
+        int L = tempFirst.length();
+        int removedPairsCount = (n - L) / 2;
+        score += removedPairsCount * Math.max(x, y);
+
+        // Second Pass
+        String tempSecond = removeSubstring(tempFirst, minStr);
+        removedPairsCount = (L - tempSecond.length()) / 2;
+        score += removedPairsCount * Math.min(x, y);
+
+        return score;
+    }
+
+    private String removeSubstring(String inputString, String matchStr) {
+        StringBuilder sb = new StringBuilder();
+        int j = 0;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            sb.append(inputString.charAt(i));
+            j++;
+
+            if (j > 1 && sb.charAt(j - 2) == matchStr.charAt(0) && sb.charAt(j - 1) == matchStr.charAt(1)) {
+                sb.delete(j - 2, j);
+                j -= 2;
+            }
+        }
+
+        return sb.toString();
+    }
+}
