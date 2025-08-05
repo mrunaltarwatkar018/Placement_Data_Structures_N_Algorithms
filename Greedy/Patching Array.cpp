1447b29e5f0ea6c831a4437ec55885e218209917
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-08-05 12:25:06 +0530

    Problem No.: 330. Patching Array

---
+++ Greedy/Patching Array.cpp
@@ -0,0 +1,57 @@
+/* Leetcode Problem No.: 330. Patching Array  */
+
+
+/*
+    Company Tags                : GOOGLE
+    Leetcode Link               : https://leetcode.com/problems/patching-array/description/
+*/
+
+
+/************************************************** C++ **************************************************/
+//Approach (Greedy observation)
+//T.C : O(max(l, log(n)), l = length of nums
+//S.C : O(1)
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long maxReach = 0;
+        int patch     = 0;
+        int i         = 0;
+
+        while(maxReach < n) {
+            if(i < nums.size() && nums[i] <= maxReach+1) {
+                maxReach += nums[i];
+                i++;
+            } else {
+                maxReach += (maxReach + 1);
+                patch++;
+            }
+        }
+        return patch;
+    }
+};
+
+
+
+/************************************************** JAVA **************************************************/
+//Approach (Greedy observation)
+//T.C : O(max(l, log(n)), l = length of nums
+//S.C : O(1)
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long maxReach = 0;
+        int patch = 0;
+        int i = 0;
+
+        while (maxReach < n) {
+            if (i < nums.length && nums[i] <= maxReach + 1) {
+                maxReach += nums[i];
+                i++;
+            } else {
+                maxReach += (maxReach + 1);
+                patch++;
+            }
+        }
+        return patch;
+    }
+}
