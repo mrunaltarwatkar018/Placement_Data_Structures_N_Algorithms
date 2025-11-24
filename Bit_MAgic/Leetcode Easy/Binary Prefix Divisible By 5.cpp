44d72e67e3b36a036dee891954de26f4116fd962
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-11-24 12:02:23 +0530

    Problem No.: 1018. Binary Prefix Divisible By 5

---
+++ Bit_MAgic/Leetcode Easy/Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,51 @@
+/* Leetcode Problem No.: 1018. Binary Prefix Divisible By 5  */
+
+/*
+    Company Tags                : will update later
+    Leetcode Link               : https://leetcode.com/problems/binary-prefix-divisible-by-5
+*/
+
+/****************************************************************************** C++ ******************************************************************************/
+//Approach (Using bit concept and modular arithmetic)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> result(n);
+
+        int remain = 0;
+        
+        for(int i = 0; i < n; i++) {
+            remain = (remain*2 + nums[i]) % 5;
+
+            result[i] = (remain == 0);
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/****************************************************************************** JAVA ******************************************************************************/
+//Approach (Using bit concept and modular arithmetic)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        int n = nums.length;
+        List<Boolean> result = new ArrayList<>(n);
+
+        int remain = 0;
+
+        for (int i = 0; i < n; i++) {
+            remain = (remain * 2 + nums[i]) % 5;
+            result.add(remain == 0);
+        }
+
+        return result;
+    }
+}
