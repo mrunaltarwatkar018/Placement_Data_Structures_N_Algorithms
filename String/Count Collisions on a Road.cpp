54e6ef8c72555d34ee34095f7a526f5fe31e719d
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-12-04 15:48:49 +0530

    Problem No.: 2211. Count Collisions on a Road

---
+++ String/Count Collisions on a Road.cpp
@@ -0,0 +1,70 @@
+/* Leetcode Problem No.: 2211. Count Collisions on a Road  */
+
+/*
+    Company Tags                : will update soon
+    Leetcode Link               : https://leetcode.com/problems/count-collisions-on-a-road
+*/
+
+
+/**************************************************************** C++ ****************************************************************/
+//Approach (Simple iteration and simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.length();
+
+        int i = 0; //left boundary
+        while(i < n && directions[i] == 'L') {
+            i++;
+        }
+
+        int j = n-1;
+        while(j >= 0 && directions[j] == 'R') {
+            j--;
+        }
+
+        int collisions = 0;
+        while(i <= j) {
+            if(directions[i] != 'S') {
+                collisions++;
+            }
+            i++;
+        }
+
+        return collisions;
+    }
+};
+
+
+
+/**************************************************************** JAVA ***************************************************************/
+//Approach (Simple iteration and simulation)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+
+        int i = 0; // left boundary
+        while (i < n && directions.charAt(i) == 'L') {
+            i++;
+        }
+
+        int j = n - 1; // right boundary
+        while (j >= 0 && directions.charAt(j) == 'R') {
+            j--;
+        }
+
+        int collisions = 0;
+        while (i <= j) {
+            if (directions.charAt(i) != 'S') {
+                collisions++;
+            }
+            i++;
+        }
+
+        return collisions;
+    }
+}
