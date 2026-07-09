3f933527c2d6895d55252a688d6fa71ce7187d5a
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2026-07-09 13:49:54 +0530

    Problem No.: 3756. Concatenate Non-Zero Digits and Multiply by Sum II

--- Arrays/Cumulative Sum (Prefix Array)/Concatenate Non-Zero Digits and Multiply by Sum II.cpp
+++ Arrays/Cumulative Sum (Prefix Array)/Concatenate Non-Zero Digits and Multiply by Sum II.cpp
@@ -1,6 +1,5 @@
 /* Leetcode Problem No.: 3756. Concatenate Non-Zero Digits and Multiply by Sum II  */
 
-
 /*
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-ii/
