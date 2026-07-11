e8d237cb310adedd87dc0d0c9da2f22f188ea8a3
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2026-07-11 13:16:03 +0530

    Problem No.: 3534. Path Existence Queries in a Graph II

---
+++ Binary Lifiting/Path Existence Queries in a Graph II.cpp
@@ -0,0 +1,211 @@
+/* Leetcode Problem No.: 3534. Path Existence Queries in a Graph II  */
+
+/*
+    Company Tags                : Will update later
+    Leetcode Link               : https://leetcode.com/problems/path-existence-queries-in-a-graph-ii/
+*/
+
+
+/******************************************************************************* C++ ************************************************************************************************/
+//Approach (Using Binary Lifting variation)
+//T.C : O(N·log N + Q·log N)
+//S.C : O(N·log N)
+class Solution {
+public:
+    int rows;
+    int cols;
+    vector<vector<int>> ancestorTable;
+
+    int customUpperBound(vector<pair<int, int>>& arr, int target) {
+        int n = arr.size();
+        int l = 0;
+        int r = n-1;
+        int result = 0;
+
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(arr[mid].first <= target) {
+                result = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+
+        return result;
+    }
+
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        //arr
+        vector<pair<int, int>> arr(n);
+        for(int i = 0; i < n; i++) {
+            arr[i] = {nums[i], i};
+        }
+
+        sort(begin(arr), end(arr));
+        vector<int> nodeToIdx(n);
+        for(int i = 0; i < n; i++) {
+            int node = arr[i].second;
+            nodeToIdx[node] = i;
+        }
+
+        rows = n;
+        cols = log2(n)+1;
+        ancestorTable.resize(rows, vector<int>(cols, 0));
+
+        //Fill 0th column first
+        for(int node = 0; node < n; node++) { //nlogn
+            int farthestIdxOneHop = customUpperBound(arr, arr[node].first + maxDiff);
+            ancestorTable[node][0] = farthestIdxOneHop;
+        }
+
+        //Fill remaining column
+        for(int j = 1; j < cols; j++) { //logn
+            for(int node = 0; node < n; node++) { //n
+                ancestorTable[node][j] = ancestorTable[ ancestorTable[node][j-1] ][j-1];
+            }
+        }
+
+        vector<int> result;
+        for(auto &query : queries) { //O(q)
+            int u = query[0];
+            int v = query[1];
+
+            int a = nodeToIdx[u];
+            int b = nodeToIdx[v];
+            if(a == b) {
+                result.push_back(0);
+                continue;
+            }
+
+            if(a > b) {
+                swap(a, b);
+            }
+
+            int curr  = a;
+            int jumps = 0;
+
+            for(int j = cols-1; j >= 0; j--) { //log(n)
+                if(ancestorTable[curr][j] < b) {
+                    curr = ancestorTable[curr][j];
+                    jumps += (1 << j); //pow(2, j)
+                }
+            }
+
+            if(ancestorTable[curr][0] >= b) {
+                result.push_back(jumps+1);
+            } else {
+                result.push_back(-1);
+            }
+            
+        }
+
+        return result;
+
+    }
+};
+
+
+
+/******************************************************************************* JAVA ************************************************************************************************/
+//Approach (Using Binary Lifting variation)
+//T.C : O(N·log N + Q·log N)
+//S.C : O(N·log N)
+class Solution {
+    int rows;
+    int cols;
+    int[][] ancestorTable;
+
+    private int customUpperBound(int[][] arr, int target) {
+        int n = arr.length;
+        int l = 0;
+        int r = n - 1;
+        int result = 0;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid][0] <= target) {
+                result = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        // arr : {value, originalIndex}
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (x, y) -> Integer.compare(x[0], y[0]));
+
+        int[] nodeToIdx = new int[n];
+        for (int i = 0; i < n; i++) {
+            int node = arr[i][1];
+            nodeToIdx[node] = i;
+        }
+
+        rows = n;
+        cols = (int) (Math.log(n) / Math.log(2)) + 1;
+        ancestorTable = new int[rows][cols];
+
+        // Fill 0th column first
+        for (int node = 0; node < n; node++) { // nlogn
+            int farthestIdxOneHop = customUpperBound(arr, arr[node][0] + maxDiff);
+            ancestorTable[node][0] = farthestIdxOneHop;
+        }
+
+        // Fill remaining columns
+        for (int j = 1; j < cols; j++) { // logn
+            for (int node = 0; node < n; node++) { // n
+                ancestorTable[node][j] = ancestorTable[ ancestorTable[node][j - 1] ][j - 1];
+            }
+        }
+
+        int[] result = new int[queries.length];
+        int idx = 0;
+        for (int[] query : queries) { // O(q)
+            int u = query[0];
+            int v = query[1];
+
+            int a = nodeToIdx[u];
+            int b = nodeToIdx[v];
+            if (a == b) {
+                result[idx++] = 0;
+                continue;
+            }
+
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            int curr  = a;
+            int jumps = 0;
+
+            for (int j = cols - 1; j >= 0; j--) { // log(n)
+                if (ancestorTable[curr][j] < b) {
+                    curr = ancestorTable[curr][j];
+                    jumps += (1 << j); // pow(2, j)
+                }
+            }
+
+            if (ancestorTable[curr][0] >= b) {
+                result[idx++] = jumps + 1;
+            } else {
+                result[idx++] = -1;
+            }
+        }
+
+        return result;
+    }
+}
