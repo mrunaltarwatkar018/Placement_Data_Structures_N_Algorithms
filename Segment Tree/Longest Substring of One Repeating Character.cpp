22f1fb0f33c8dd9145d3293393fbd6e6a03eff63
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2026-08-16 13:15:50 +0530

    Problem No.: 2213 . Longest Substring of One Repeating Character

--- Segment Tree/Longest Substring of One Repeating Character.cpp
+++ Segment Tree/Longest Substring of One Repeating Character.cpp
@@ -5,10 +5,6 @@
     Leetcode Link               : https://leetcode.com/problems/longest-substring-of-one-repeating-character/
 */
 
-
-
-
-
 /**************************************************************** C++ ****************************************************************/
 //Approach (Segment Tree)
 //T.C : O(n + klog(n)), build: O(n) and each update: O(log n), done k times → O(k·log n)
